41ea003ac887fef524350d0e256bd2b1a00bed85
Jie Qiang Tan <jieqiangt@gmail.com>
2022-12-05 21:15:14 +0800

    added font for page. renamed nav.module.scss to layout.module.scss. added animation for underlining link after clicking.

--- components/layout/NavBar.js
+++ components/layout/NavBar.js
@@ -1,10 +1,11 @@
 import NavLink from "./NavLink";
 import Logo from "../ui/Logo";
-import classes from "./sass/nav.module.scss";
+import classes from "./sass/layout.module.scss";
+import { Fragment } from "react";
 
 export default function NavBar() {
   return (
-    <nav className={classes['nav']}>
+    <nav className={classes["nav"]}>
       <ul className={classes["nav--list"]}>
         <NavLink href="/discover">Our Story</NavLink>
         <NavLink href="/new">Upcoming Retreats</NavLink>
--- components/layout/NavLink.js
+++ components/layout/NavLink.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import classes from "./sass/nav.module.scss";
+import classes from "./sass/layout.module.scss";
 
 export default function NavLink(props) {
   return (
