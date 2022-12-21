b5518db7945e2767b6bf5fd1224967b31aef330b
jieqiangt <jieqiangt@gmail.com>
2022-12-21 15:43:17 +0800

    - removed test code for principle section - renamed test2 component to principle

--- components/discover/GuidingPrinciple.js
+++ components/discover/GuidingPrinciple.js
@@ -1,21 +1,17 @@
-import { Fragment } from "react";
 import classes from "../../sass/pages/discover.module.scss";
 
 export default function GuidingPrinciple() {
   return (
-    <Fragment>
-      <section className={classes["principle"]}>
-        <div className={classes["principle--box"]}>
-          <h2 className={classes["principle--title"]}>Our Guiding Principle</h2>
-          <article className={classes["principle--article"]}>
-            Living life with unwavering faith and joy, <br />
-            as the{" "}
-            <span className={classes["principle--lotus--em"]}>lotus </span>
-            does, <br />
-            to ensure the most beautiful revival
-          </article>
-        </div>
-      </section>
-    </Fragment>
+    <section className={classes["principle"]}>
+      <div className={classes["principle--box"]}>
+        <h2 className={classes["principle--title"]}>Our Guiding Principle</h2>
+        <p className={classes["principle--desc"]}>
+          Living life with unwavering faith and joy, <br />
+          as the
+          <span className={classes["principle--em"]}> lotus</span> does, <br />
+          to ensure the most beautiful revival
+        </p>
+      </div>
+    </section>
   );
 }
--- pages/discover/index.js
+++ pages/discover/index.js
@@ -3,8 +3,6 @@ import { Fragment } from "react";
 import GuidingPrinciple from "../../components/discover/GuidingPrinciple";
 import OurApproach from "../../components/discover/OurApproach";
 import OurFounder from "../../components/discover/OurFounder";
-import Test from "../../components/discover/Test";
-import Test2 from "../../components/discover/Test2";
 import TheWayOfLotus from "../../components/discover/TheWayOfLotus";
 import Footer from "../../components/layout/Footer";
 import Layout from "../../components/layout/Layout";
@@ -21,8 +19,7 @@ export default function DiscoverPage() {
       // firstSection={GuidingPrinciple}
       // headerClass={classes["discover--header"]}
       >
-        {/* <Test /> */}
-        <Test2 />
+        <GuidingPrinciple />
         <TheWayOfLotus />
         <OurApproach />
         <OurFounder />
