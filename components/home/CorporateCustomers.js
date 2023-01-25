78068ff8ef1728f820afb6590b2c47a397114c0e
jayyqueue <jieqiangt@gmail.com>
2023-01-25 21:22:13 +0800

    - added logos in corporate customers component

--- components/home/CorporateCustomers.js
+++ components/home/CorporateCustomers.js
@@ -1,4 +1,33 @@
+import Icon from "../ui/Icon";
+
 export default function CorporateCustomers(props) {
   const { baseClass, classes } = props;
-  return <section className={classes[baseClass]}>&nbsp;</section>;
+  return (
+    <section className={classes[baseClass]}>
+      <aside className={classes[`${baseClass}--clients`]}>
+        <h3 className={classes[`${baseClass}--title`]}>Corporate Clients</h3>
+        <div className={classes[`${baseClass}--icons`]}>
+          <Icon
+            iconClass={`${classes[`${baseClass}--icon`]} ${
+              classes[`${baseClass}--icon--accenture`]
+            } hidden`}
+            iconName="icon-accenture"
+          />
+        </div>
+      </aside>
+      <aside className={classes[`${baseClass}--partners`]}>
+        <h3 className={classes[`${baseClass}--title`]}>Welleness Partners</h3>
+        <div className={classes[`${baseClass}--icons`]}>
+          <Icon
+            iconClass={`${classes[`${baseClass}--icon`]} hidden`}
+            iconName="icon-oasia"
+          />
+          <Icon
+            iconClass={`${classes[`${baseClass}--icon`]} hidden`}
+            iconName="icon-raffles"
+          />
+        </div>
+      </aside>
+    </section>
+  );
 }
