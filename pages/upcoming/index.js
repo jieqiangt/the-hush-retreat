449be6ee1f5399a2948c5884b7aed48f51b63a2d
jayyqueue <jieqiangt@gmail.com>
2023-03-09 17:13:11 +0800

    - reogranized upcoming page - added details page for new retreat

--- pages/upcoming/index.js
+++ pages/upcoming/index.js
@@ -1,29 +1,17 @@
 import Head from "next/head";
 import { Fragment } from "react";
 import Layout from "../../components/layout/Layout";
-import NewRetreatBanner from "../../components/new/NewRetreatBanner";
-import NewRetreatBookingModal from "../../components/new/NewRetreatBookingModal";
-import NewRetreatExpect from "../../components/new/NewRetreatExpect";
-import NewRetreatGuide from "../../components/new/NewRetreatGuide";
-import NewRetreatIntentions from "../../components/new/NewRetreatIntentions";
-import NewRetreatSchedule from "../../components/new/NewRetreatSchedule";
 import classes from "../../sass/pages/upcoming.module.scss";
 
 export default function UpcomingPage() {
-  const modal = <NewRetreatBookingModal classes={classes} baseClass="book" />;
-
   return (
     <Fragment>
       <Head>
         <title>New Retreats</title>
         <meta name="description" content="New Retreats" />
       </Head>
-      <Layout classes={classes} baseClass="upcoming" modal={modal}>
-        <NewRetreatBanner classes={classes} baseClass="retreat--banner" />
-        <NewRetreatIntentions classes={classes} baseClass="intentions" />
-        <NewRetreatExpect classes={classes} baseClass="expect" />
-        <NewRetreatGuide classes={classes} baseClass="guide" />
-        <NewRetreatSchedule classes={classes} baseClass="schedule" />
+      <Layout classes={classes} baseClass="upcoming">
+        
       </Layout>
     </Fragment>
   );
