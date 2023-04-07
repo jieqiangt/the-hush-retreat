ae00873cc16c388432d747fac0d21b878100c106
jayyqueue <jieqiangt@gmail.com>
2023-04-07 18:40:25 +0800

    - edited bookingSchema to correct schema - added retreatSchema

--- models/bookingSchema.js
+++ models/bookingSchema.js
@@ -10,13 +10,11 @@ module.exports = {
         $jsonSchema: {
           bsonType: "object",
           required: [
-            "retreatName",
-            "email",
-            "firstName",
-            "lastName",
-            "phone",
-            "vaccinated",
-            "numRetreatees",
+            "createdOn",
+            "updatedOn",
+            "retreatId",
+            "mainRetreatee",
+            "status",
           ],
           additionalProperties: false,
           properties: {
@@ -31,47 +29,97 @@ module.exports = {
               description:
                 "updatedOn of iso date type is required. Must be greater than or equals to createdOn.",
             },
-            retreatName: {
-              bsonType: "string",
-              description: "retreatName of string datatype is required.",
+            retreatId: {
+              bsonType: "objectId",
+              description: "references objectId of restaurants in the list.",
             },
-            firstName: {
-              bsonType: "string",
-              description: "firstName of string datatype is required.",
+            mainRetreatee: {
+              bsonType: "object",
+              properties: {
+                firstName: {
+                  bsonType: "string",
+                  description: "firstName of string datatype is required.",
+                },
+                lastName: {
+                  bsonType: "string",
+                  description: "lastName of string datatype is required.",
+                },
+                email: {
+                  bsonType: "string",
+                  maxLength: 50,
+                  description: "email of string datatype is required.",
+                },
+                phone: {
+                  bsonType: "string",
+                  maxLength: 15,
+                  description: "phone of string datatype is required.",
+                },
+                vaccinated: {
+                  bsonType: "bool",
+                  description: "vaccinated of boolean data type is required.",
+                },
+                retreateeId: {
+                  bsonType: "string",
+                  description:
+                    "retreateeId of string datatype is required. Must be main for main retreatee",
+                },
+              },
             },
-            lastName: {
-              bsonType: "string",
-              description: "lastName of string datatype is required.",
-            },
-            email: {
-              bsonType: "string",
-              maxLength: 50,
-              description: "email of string datatype is required.",
-            },
-            phone: {
-              bsonType: "string",
-              maxLength: 15,
-              description: "phone of string datatype is required.",
-            },
-            vaccinated: {
-              bsonType: "bool",
-              description: "vaccinated of boolean data type is required.",
-            },
-            numRetreatees: {
-              bsonType: "int",
-              minimum: 1,
-              maximum: 8,
-              description:
-                "numRetreatees of int type is required and must be between 1 & 8.",
-            },
-            bookingStatus: {
-              bsonType: "string",
-              enum: ["pendingEmail", "emailSent", "emailAcknowledged", "paid"],
-              description: "bookingStatus of string data type is required.",
+            additionalRetreatees: {
+              bsonType: "array",
+              uniqueItems: true,
+              additionalProperties: false,
+              items: {
+                bsonType: "object",
+                properties: {
+                  firstName: {
+                    bsonType: "string",
+                    description: "firstName of string datatype is required.",
+                  },
+                  lastName: {
+                    bsonType: "string",
+                    description: "lastName of string datatype is required.",
+                  },
+                  email: {
+                    bsonType: "string",
+                    maxLength: 50,
+                    description: "email of string datatype is required.",
+                  },
+                  phone: {
+                    bsonType: "string",
+                    maxLength: 15,
+                    description: "phone of string datatype is required.",
+                  },
+                  vaccinated: {
+                    bsonType: "bool",
+                    description: "vaccinated of boolean data type is required.",
+                  },
+                  retreateeId: {
+                    bsonType: "string",
+                    description:
+                      "retreateeId of string datatype is required. Must be main for main retreatee",
+                  },
+                },
+              },
             },
             message: {
               bsonType: "string",
+              maxLength: 500,
               description: "message of string data type is optional.",
+            },
+            status: {
+              bsonType: "string",
+              enum: [
+                "bookingRegistered",
+                "bookingConfirmationSent",
+                "depositReceived",
+                "depositConfirmationSent",
+                "fullPaymentReminderSent",
+                "fullPaymentReceived",
+                "paymentConfirmationSent",
+                "preRetreatPrepEmailSent",
+              ],
+              description: "status of string data type is required.",
             },
           },
         },
---
+++ models/retreatSchema.js
@@ -0,0 +1,61 @@
+module.exports = {
+  validator: {
+    $and: [
+      {
+        $expr: {
+          $gte: ["$updatedOn", "$createdOn"],
+        },
+      },
+      {
+        $jsonSchema: {
+          bsonType: "object",
+          required: [
+            "createdOn",
+            "updatedOn",
+            "retreatName",
+            "startDt",
+            "endDt",
+            "price",
+            "location",
+          ],
+          additionalProperties: false,
+          properties: {
+            _id: { bsonType: "objectId" },
+            createdOn: {
+              bsonType: "date",
+              description:
+                "createdOn of iso date type is required. Defaults to booking date.",
+            },
+            updatedOn: {
+              bsonType: "date",
+              description:
+                "updatedOn of iso date type is required. Must be greater than or equals to createdOn.",
+            },
+            retreatName: {
+              bsonType: "string",
+              description: "retreatName of string datatype is required.",
+            },
+            startDt: {
+              bsonType: "string",
+              description:
+                "startDt of string datatype is required. This is in the human readable format to be shown in website.",
+            },
+            endDt: {
+              bsonType: "string",
+              description:
+                "startDt of string datatype is required. This is in the human readable format to be shown in website.",
+            },
+            price: {
+              bsonType: "string",
+              description: "price of string datatype without $ is required.",
+            },
+            location: {
+              bsonType: "string",
+              description: "location of string datatype is required.",
+            },
+          },
+        },
+      },
+    ],
+  },
+};
