f905f2cb98323ea885b7bf00c9d1a06908a25d49
jayyqueue <jieqiangt@gmail.com>
2023-04-12 23:42:15 +0800

    - refactored NewRetreatSection to be able to use imgComposition component

--- components/upcoming/NewRetreatSection.js
+++ components/upcoming/NewRetreatSection.js
@@ -1,19 +1,28 @@
 import DescriptionSection from "../ui/DescriptionSection";
+import ImgComposition from "../ui/ImgComposition";
 
 export default function NewRetreatSection(props) {
-  const { classes, baseClass, title, text } = props;
+  const { classes, baseClass, title, text, imgUrlList } = props;
 
   const paragraphs = text.map((item, idx) => <p key={idx}>{item}</p>);
   const content = (
-    <article
-      className={`${
-        classes[`${baseClass}--aside--content`]
-      } hidden`}
-    >
+    <article className={`${classes[`${baseClass}--aside--content`]} hidden`}>
       {paragraphs}
     </article>
   );
-  const img = <div className={`${classes[`${baseClass}--img`]} hidden`} />;
+
+  const img = imgUrlList ? (
+    <div className={classes[`${baseClass}--img`]}>
+      <ImgComposition
+        classes={classes}
+        baseClass={baseClass}
+        imgUrlList={imgUrlList}
+      />
+    </div>
+  ) : (
+    <div className={`${classes[`${baseClass}--img`]} hidden`} />
+  );
+
 
   return (
     <DescriptionSection
