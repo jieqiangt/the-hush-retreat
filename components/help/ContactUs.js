63d9a553a97e462e14d9a0060b72210f416a704a
jayyqueue <jieqiangt@gmail.com>
2023-04-25 15:53:26 +0800

    - revamped Contact Us form to have ref to get data submitted

--- components/help/ContactUs.js
+++ components/help/ContactUs.js
@@ -1,9 +1,116 @@
 import Link from "next/link";
 import DescriptionSection from "../ui/DescriptionSection";
 import InputText from "../ui/InputText";
+import { useContext, useRef, useState } from "react";
+import { callApi } from "../../utils/apiUtils";
+import NotificationContext from "../../store/notificationContext";
+import { validateField } from "../../reducers/newRetreateeReducer";
+import LinkButton from "../ui/LinkButton";
 
 export default function ContactUs(props) {
   const { baseClass, classes } = props;
+
+  const [emailValid, setEmailValid] = useState(true);
+  const [firstNameValid, setFirstNameValid] = useState(true);
+  const [lastNameValid, setLastNameValid] = useState(true);
+  const [subjectValid, setSubjectValid] = useState(true);
+  const [messageValid, setMessageValid] = useState(true);
+
+  const notificationCtx = useContext(NotificationContext);
+  const emailRef = useRef();
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const subjectRef = useRef();
+  const messageRef = useRef();
+
+  async function submitHandler(event) {
+    event.preventDefault();
+
+    let escape = 0;
+
+    if (!validateField(emailRef.current.value, "email")) {
+      setEmailValid(() => false);
+      escape = escape + 1;
+    } else {
+      setEmailValid(() => true);
+    }
+
+    if (!validateField(firstNameRef.current.value, "firstName")) {
+      setFirstNameValid(() => false);
+      escape = escape + 1;
+    } else {
+      setFirstNameValid(() => true);
+    }
+
+    if (!validateField(lastNameRef.current.value, "lastName")) {
+      setLastNameValid(() => false);
+      escape = escape + 1;
+    } else {
+      setLastNameValid(() => true);
+    }
+
+    if (!validateField(subjectRef.current.value, "standardString")) {
+      setSubjectValid(() => false);
+      escape = escape + 1;
+    } else {
+      setSubjectValid(() => true);
+    }
+
+    if (!validateField(messageRef.current.value, "standardString")) {
+      console.log("validation failed");
+      setMessageValid(() => false);
+      escape = escape + 1;
+    } else {
+      setMessageValid(() => true);
+    }
+
+    if (escape > 0) {
+      return;
+    }
+
+    const result = await callApi({
+      url: "/api/registerContactUsMessage",
+      method: "POST",
+      body: {
+        email: emailRef.current.value,
+        firstName: firstNameRef.current.value,
+        lastName: lastNameRef.current.value,
+        subject: subjectRef.current.value,
+        message: messageRef.current.value,
+      },
+    });
+
+    if (!result.ok) {
+      const errorNotification = await result.json();
+      notificationCtx.showNotification(errorNotification);
+    }
+
+    if (result.ok) {
+      const successNotification = await result.json();
+
+      await callApi({
+        url: "/api/sendContactUsConfirmation",
+        method: "POST",
+        body: {
+          email: emailRef.current.value,
+          firstName: firstNameRef.current.value,
+          lastName: lastNameRef.current.value,
+          subject: subjectRef.current.value,
+          message: messageRef.current.value,
+          referenceId: successNotification.referenceId,
+          insertedId: successNotification.insertedId,
+        },
+      });
+
+      notificationCtx.showNotification(successNotification);
+
+      emailRef.current.value = "";
+      firstNameRef.current.value = "";
+      lastNameRef.current.value = "";
+      subjectRef.current.value = "";
+      messageRef.current.value = "";
+    }
+  }
 
   const content = (
     <form action="#" className={classes[`${baseClass}--form`]}>
@@ -16,54 +123,72 @@ export default function ContactUs(props) {
         <Link href="#faq">FAQ section</Link> below. <br />
       </article>
       <InputText
+        label="Email"
+        type="email"
+        inputRef={emailRef}
+        inputName="newsletter--form--signup--email"
+        inputPlaceholder="Email"
+        inputGroupClass={classes[`${baseClass}--form--email`]}
+        inputClass={classes[`${baseClass}--form--email--input`]}
+        labelClass={classes[`${baseClass}--form--email--label`]}
+        valid={emailValid}
+        invalidText="A valid email is required."
+      />
+      <InputText
         label="First Name"
         type="text"
+        inputRef={firstNameRef}
         inputName="contact-us--form--first-name"
         inputPlaceholder="First Name"
         inputGroupClass={classes[`${baseClass}--form--first-name`]}
         inputClass={classes[`${baseClass}--form--first-name--input`]}
         labelClass={classes[`${baseClass}--form--first-name--label`]}
+        valid={firstNameValid}
+        invalidText="Your first name is required."
       />
       <InputText
         label="Last Name"
         type="text"
+        inputRef={lastNameRef}
         inputName="contact-us--form--last-name"
         inputPlaceholder="Last Name"
         inputGroupClass={classes[`${baseClass}--form--last-name`]}
         inputClass={classes[`${baseClass}--form--last-name--input`]}
         labelClass={classes[`${baseClass}--form--last-name--label`]}
-      />
-      <InputText
-        label="Email"
-        type="email"
-        inputName="contact-us--form--email"
-        inputPlaceholder="Email"
-        inputGroupClass={classes[`${baseClass}--form--email`]}
-        inputClass={classes[`${baseClass}--form--email--input`]}
-        labelClass={classes[`${baseClass}--form--email--label`]}
+        valid={lastNameValid}
+        invalidText="Your last name is required."
       />
       <InputText
         label="Subject"
         type="text"
+        inputRef={subjectRef}
         inputName="contact-us--form--subject"
         inputPlaceholder="Subject"
         inputGroupClass={classes[`${baseClass}--form--subject`]}
         inputClass={classes[`${baseClass}--form--subject--input`]}
         labelClass={classes[`${baseClass}--form--subject--label`]}
+        valid={subjectValid}
+        invalidText="A subject is required."
       />
       <InputText
         label="Message"
         type="textarea"
+        inputRef={messageRef}
         inputName="contact-us--form--message"
         inputPlaceholder="Message"
         inputGroupClass={classes[`${baseClass}--form--message`]}
         inputClass={classes[`${baseClass}--form--message--input`]}
         labelClass={classes[`${baseClass}--form--message--label`]}
+        valid={messageValid}
+        invalidText="A message is required."
         rows="8"
       />
-      <button className={classes[`${baseClass}--form--btn`]}>
-        Send Message
-      </button>
+      <LinkButton
+        btnClass={classes[`${baseClass}--form--btn`]}
+        onClick={submitHandler}
+      >
+        Submit
+      </LinkButton>
     </form>
   );
 
