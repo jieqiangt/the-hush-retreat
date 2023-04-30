7b3eddc9c8b0e4e537c025f82eb0b2ce9c7f1479
jayyqueue <jieqiangt@gmail.com>
2023-04-30 11:52:24 +0800

    - formatted email desc to go to another line

--- pages/api/emailTemplates/RetreatConfirmationTemplate.js
+++ pages/api/emailTemplates/RetreatConfirmationTemplate.js
@@ -22,7 +22,8 @@ export default function RetreatConfirmationTemplate(props) {
         <p style={textStyle}>This is Mandi from The Hush Retreats! </p>
         <p
           style={textStyle}
-        >{`Thank you for expressing interest to spend a lovely time with us at ${retreat.name} on ${retreat.date}. This will be ${retreat.emailDesc}.`}</p>
+        >{`Thank you for expressing interest to spend a lovely time with us at ${retreat.name} on ${retreat.date}.`}</p>
+        <p>{`This will be ${retreat.emailDesc}.`}</p>
       </div>
       <div style={divStyle}>
         <h3 style={headerStyle}>Main Retreatee</h3>
@@ -48,14 +49,6 @@ export default function RetreatConfirmationTemplate(props) {
           )}
         </ul>
       </div>
-      {retreat.emailOtherInfo ? (
-        <div style={divStyle}>
-          <p style={textStyle}>Here are your booking details:</p>
-          <p style={textStyle}>{retreat.emailOtherInfo}</p>
-        </div>
-      ) : (
-        ""
-      )}
       <div style={divStyle}>
         <p style={textStyle}>
           You may find attached a Participant Information & Indemnity Form that
