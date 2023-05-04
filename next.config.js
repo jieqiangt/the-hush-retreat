dc8e17d74ecc45c31f4c04bcd9d8a15d6af9cfa3
jayyqueue <jieqiangt@gmail.com>
2023-05-04 15:34:32 +0800

    - ignoring .sh & .pem files in docker & git - removing caching from next js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "standalone",
   images: {
-    minimumCacheTTL: 60,
+    minimumCacheTTL: 0,
+  },
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png|webp)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
   },
 };
 
