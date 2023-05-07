0b158a0bf73beba35e56850bdf9ddad39428ea8d
jayyqueue <jieqiangt@gmail.com>
2023-05-07 20:46:29 +0800

    - added shell script for ec2 configuration

---
+++ ec2-configure-ami.sh
@@ -0,0 +1,39 @@
+
+# for configuring ami
+
+sudo su
+
+yum update -y
+amazon-linux-extras install docker
+
+yum install amazon-cloudwatch-agent
+amazon-linux-extras install collectd
+
+mkdir -p /user/share/collectd
+touch /user/share/collectd/types.db
+
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard # only required for configuring the first time)
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:AmazonCloudWatch-ec2linux-no-logs -s
+service docker start
+
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` \
+&& INSTANCE_ID=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id`
+aws ec2 modify-instance-metadata-options --instance-id $INSTANCE_ID --http-put-response-hop-limit 2 --http-endpoint enabled
+
+# for user data script
+
+#!/bin/bash
+sudo docker pull jieqiangtan/hush-retreat-prod:latest
+sudo docker run -d --rm -p 80:3000 --name hush-retreat-server \
+-e MONGO_URI=$(aws ssm get-parameter --name MONGO_URI | jq -r '.Parameter' | jq -r '.Value') \
+-e MONGO_USER=$(aws ssm get-parameter --name MONGO_USER | jq -r '.Parameter' | jq -r '.Value') \
+-e MONGO_DBNAME=$(aws ssm get-parameter --name MONGO_DBNAME | jq -r '.Parameter' | jq -r '.Value') \
+-e MONGO_PW=$(aws ssm get-parameter --name MONGO_PW --with-decryption | jq -r '.Parameter' | jq -r '.Value') \
+jieqiangtan/hush-retreat-prod:latest
+
+
+
+
+# to go into docker container
+
+docker exec -it hush-retreat-server sh
