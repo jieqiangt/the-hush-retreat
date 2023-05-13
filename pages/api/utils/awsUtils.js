df6c3034bf3a6e9f5490f8c868094aa4ce2d4496
jayyqueue <jieqiangt@gmail.com>
2023-05-13 15:09:01 +0800

    - added try catch to catch and log errors

--- pages/api/utils/awsUtils.js
+++ pages/api/utils/awsUtils.js
@@ -3,15 +3,27 @@ import { InvokeCommand, LambdaClient } from "@aws-sdk/client-lambda";
 const REGION = "ap-southeast-1";
 
 export async function invokeAsyncLambda(fnArn, payload) {
-  const client = new LambdaClient({ region: REGION });
+  try {
+    const client = new LambdaClient({ region: REGION });
+    const input = {
+      FunctionName: fnArn,
+      InvocationType: "Event",
+      Payload: JSON.stringify(payload),
+    };
 
-  const input = {
-    FunctionName: fnArn,
-    InvocationType: "Event",
-    Payload: JSON.stringify(payload),
-  };
-
-  const result = await client.send(new InvokeCommand(input));
-  client.destroy();
-  return result;
+    const result = await client.send(new InvokeCommand(input));
+    client.destroy();
+    return result;
+  } catch (err) {
+    const title = "AWS Email Service Error";
+    Logger.error(`::::::::::::::: ${getCurrentDateTimeStr()} :::::::::::: `);
+    Logger.error(title);
+    Logger.error(err);
+    throw new AppError({
+      title,
+      clientMessage: `Services is currently unavailable. Please try again later. Apologies on the inconvenience caused.`,
+      status: 503,
+      className: "notification--error",
+    });
+  }
 }
