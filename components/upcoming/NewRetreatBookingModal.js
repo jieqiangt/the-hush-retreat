664101dcdf16576a94a59a2881e67a72b9b6a0b4
jayyqueue <jieqiangt@gmail.com>
2023-05-17 17:39:58 +0800

    - shifted retreat information into booking form

--- components/upcoming/NewRetreatBookingModal.js
+++ components/upcoming/NewRetreatBookingModal.js
@@ -4,35 +4,26 @@ import { Fragment } from "react";
 import NewRetreatInfo from "./NewRetreatInfo";
 
 export default function NewRetreatBookingModal(props) {
-  const { classes, baseClass, retreat, modalTitle, modalCaption, queryMsg } =
-    props;
-
-  const modalInfo = (
-    <Fragment>
-      <span className={classes[`${baseClass}--aside--info--caption`]}>
-        {modalCaption}
-      </span>
-      <div className={classes[`${baseClass}--aside--info--grid`]}>
-        <NewRetreatInfo
-          classes={classes}
-          baseClass={`${baseClass}--aside--info`}
-          retreat={retreat}
-        />
-      </div>
-    </Fragment>
-  );
+  const {
+    classes,
+    baseClass,
+    retreat,
+    modalTitle,
+    modalCaption,
+    queryMsg,
+    isModal,
+  } = props;
 
   const aside = (
-    <section className={classes[`${baseClass}--aside`]}>
-      <h3 className={classes[`${baseClass}--aside--title`]}>{modalTitle}</h3>
-      {modalInfo}
-      <BookingForm
-        baseClass={baseClass}
-        classes={classes}
-        retreat={retreat}
-        queryMsg={queryMsg}
-      />
-    </section>
+    <BookingForm
+      isModal={isModal}
+      modalTtile={modalTitle}
+      modalCaption={modalCaption}
+      baseClass={baseClass}
+      classes={classes}
+      retreat={retreat}
+      queryMsg={queryMsg}
+    />
   );
 
   const img = <div className={classes[`${baseClass}--img`]}>&nbsp;</div>;
