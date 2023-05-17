c16929eb6a8760bef8220c2bfdcb184fc1dcb306
jayyqueue <jieqiangt@gmail.com>
2023-05-17 17:41:59 +0800

    - added isModal condition to remove retreat date if it is in booking modal - added conditional statemetns to only show icon if text is available

--- components/upcoming/NewRetreatInfo.js
+++ components/upcoming/NewRetreatInfo.js
@@ -3,39 +3,58 @@ import Icon from "../ui/Icon";
 import LinkButton from "../ui/LinkButton";
 
 export default function NewRetreatInfo(props) {
-  const { classes, baseClass, onClick, btnHref, retreat } = props;
+  const { classes, baseClass, onClick, btnHref, retreat, isModal } = props;
 
   return (
     <Fragment>
-      <div className={classes[`${baseClass}--icon--outer`]}>
-        <Icon
-          iconClass={classes[`${baseClass}--icon`]}
-          iconName="icon-calendar"
-        />
-        <time className={classes[`${baseClass}--date`]}>{retreat.date}</time>
-      </div>
-      <div className={classes[`${baseClass}--icon--outer`]}>
-        <Icon iconClass={classes[`${baseClass}--icon`]} iconName="icon-clock" />
-        <time className={classes[`${baseClass}--time`]}>{retreat.time}</time>
-      </div>
-      <div className={classes[`${baseClass}--icon--outer`]}>
-        <Icon
-          iconClass={classes[`${baseClass}--icon`]}
-          iconName="icon-location-pin"
-        />
-        <span className={classes[`${baseClass}--location`]}>
-          {retreat.location}
-        </span>
-      </div>
-      <div className={classes[`${baseClass}--icon--outer`]}>
-        <Icon
-          iconClass={classes[`${baseClass}--icon`]}
-          iconName="icon-coin-dollar"
-        />
-        <span
-          className={classes[`${baseClass}--price`]}
-        >{`$${retreat.price} SGD per pax`}</span>
-      </div>
+      {retreat.date | !isModal ? (
+        <div className={classes[`${baseClass}--icon--outer`]}>
+          <Icon
+            iconClass={classes[`${baseClass}--icon`]}
+            iconName="icon-calendar"
+          />
+          <time className={classes[`${baseClass}--date`]}>{retreat.date}</time>
+        </div>
+      ) : (
+        ""
+      )}
+      {retreat.time ? (
+        <div className={classes[`${baseClass}--icon--outer`]}>
+          <Icon
+            iconClass={classes[`${baseClass}--icon`]}
+            iconName="icon-clock"
+          />
+          <time className={classes[`${baseClass}--time`]}>{retreat.time}</time>
+        </div>
+      ) : (
+        ""
+      )}
+      {retreat.location ? (
+        <div className={classes[`${baseClass}--icon--outer`]}>
+          <Icon
+            iconClass={classes[`${baseClass}--icon`]}
+            iconName="icon-location-pin"
+          />
+          <span className={classes[`${baseClass}--location`]}>
+            {retreat.location}
+          </span>
+        </div>
+      ) : (
+        ""
+      )}
+      {retreat.price ? (
+        <div className={classes[`${baseClass}--icon--outer`]}>
+          <Icon
+            iconClass={classes[`${baseClass}--icon`]}
+            iconName="icon-coin-dollar"
+          />
+          <span
+            className={classes[`${baseClass}--price`]}
+          >{`$${retreat.price} SGD per pax`}</span>
+        </div>
+      ) : (
+        ""
+      )}
       {retreat.promotion ? (
         <p className={classes[`${baseClass}--promo`]}>{retreat.promotion}</p>
       ) : (
