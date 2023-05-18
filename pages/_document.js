3084beb975c8c2c107a24720a2d77abd4a0cbf09
jayyqueue <jieqiangt@gmail.com>
2023-05-18 12:45:32 +0800

    - adedd favicon

--- pages/_document.js
+++ pages/_document.js
@@ -19,6 +19,26 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" href="/img/favicon.ico" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/android-chrome-192x192.png"
+          />
           <meta charSet="UTF-8" />
         </Head>
         <body>
